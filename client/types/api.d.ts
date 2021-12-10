3bd564d82c8a697f95073cb30d64dfafd0cfacaf
pheeque <pheeque@gmail.com>
2021-12-10 13:36:58 +0100

    Initialize dependabot/npm_and_yarn/husky-7.0.2

---
+++ client/types/api.d.ts
@@ -0,0 +1,29 @@
+
+export interface MetApiResponse {
+  benchmark: number
+  status: 'success' | 'failure'
+  query: {
+    options: Record<string, any>
+    params: Record<string, any>
+  }
+  paginate?: {
+    current_page: number
+    first_item: number
+    last_item: number
+    last_page: number
+    pages: Array<number>
+    per_page: number
+    total: number
+  }
+  data: any
+}
+
+export interface User {
+  name: string
+  job: string
+  email: string
+  phone: string
+  avatar: string
+}
+
+export type Users = Array<User>
